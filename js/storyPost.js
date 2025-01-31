a76e407b9cb61be361f4189c243487e9f79eac5c
@r-s-20 <robin_s_2020@mailbox.org>
2025-01-31 15:13:58 +0100

    adjust loading of posts for story and chapter hierarchy

--- js/storyPost.js
+++ js/storyPost.js
@@ -1,25 +1,33 @@
-let currentPost = 0;
-getCurrentPost();
+let currentChapter = 0;
+let currentStory = 0;
+getcurrentPost();
 
 window.addEventListener(
   "DOMContentLoaded",
   () => {
-    loadPost(currentPost);
+    console.log("current Post is", posts);
+    console.log("i and j in loadPost are", currentStory, currentChapter);
+    setTimeout(() => loadPost(currentStory, currentChapter), 500);
   },
   false
 );
 
-function getCurrentPost() {
-  let storedPost = JSON.parse(sessionStorage.getItem("currentPost"));
-  if (storedPost) {
-    currentPost = storedPost;
+function getcurrentPost() {
+  let storedStory = JSON.parse(sessionStorage.getItem("currentStory"));
+  let storedChapter = JSON.parse(sessionStorage.getItem("currentChapter"));
+  if (storedStory) {
+    currentStory = storedStory;
+  }
+  if (storedChapter) {
+    currentChapter = storedChapter;
   }
 }
 
-function loadPost(i) {
+function loadPost(i, j) {
+  console.log("i and j in loadPost are", i, j);
   let title = document.getElementById("postTitle");
   let content = document.getElementById("postContent");
-  let chapter = posts[0].stories[0].chapters[i];
+  let chapter = posts[0].stories[i].chapters[j];
   if (chapter) {
     title.innerHTML = chapter.title;
     content.innerHTML = chapter.content;
@@ -36,16 +44,17 @@ document.getElementById("previousPost").addEventListener("click", () => {
 });
 
 function showNextPost() {
-  currentPost = currentPost < posts[0].stories.length - 1 ? (currentPost += 1) : 0;
-  loadPost(currentPost);
-  sessionStorage.setItem("currentPost", JSON.stringify(currentPost));
+  currentChapter = currentChapter < posts[0].stories[currentStory].chapters.length - 1 ? (currentChapter += 1) : 0;
+  loadPost(currentStory, currentChapter);
+  sessionStorage.setItem("currentChapter", JSON.stringify(currentChapter));
   adjustButtonStatus();
 }
 
 function showPreviousPost() {
-  currentPost = currentPost >= 1 ? (currentPost = 0) : (currentPost = posts[0].stories.length - 1);
-  loadPost(currentPost);
-  sessionStorage.setItem("currentPost", JSON.stringify(currentPost));
+  currentChapter =
+    currentChapter >= 1 ? (currentChapter = 0) : (currentChapter = posts[0].stories[currentStory].chapters.length - 1);
+  loadPost(currentStory, currentChapter);
+  sessionStorage.setItem("currentChapter", JSON.stringify(currentChapter));
   adjustButtonStatus();
 }
 
@@ -53,12 +62,11 @@ function adjustButtonStatus() {
   let btnNext = document.getElementById("nextPost");
   let btnPrev = document.getElementById("previousPost");
 
-  if (currentPost == 0) {
+  if (currentChapter == 0) {
     btnPrev.setAttribute("disabled", "true");
   } else btnPrev.removeAttribute("disabled");
 
-  if (currentPost == posts[0].stories.length - 1) {
+  if (currentChapter == posts[0].stories[currentStory].chapters.length - 1) {
     btnNext.setAttribute("disabled", "true");
   } else btnNext.removeAttribute("disabled");
 }
-
